6176e63ff6f97ec85c07bc851c05dc33b46bca49
Evgeny <igovorov58@gmail.com>
2019-04-03 17:59:10 +0300

    Empty project

---
+++ src/main/java/com/ufu/vdata/VdataApplication.java
@@ -0,0 +1,13 @@
+package com.ufu.vdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufu/vdata/VdataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ufu.vdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VdataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
