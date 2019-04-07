2749d02e87dbcc53d573d65f679bdce87e249e33
Evgeny <igovorov58@gmail.com>
2019-04-07 12:42:26 +0300

    CRUD elections

--- src/main/java/com/ufu/vdata/controller/ElectionListController.java
+++ src/main/java/com/ufu/vdata/controller/ElectionListController.java
@@ -2,6 +2,7 @@ package com.ufu.vdata.controller;
 
 import com.ufu.vdata.entity.Election;
 import com.ufu.vdata.repository.ElectionListRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,19 +14,39 @@ import java.util.List;
 @RestController
 @RequestMapping(value= "/api/elections")
 class ElectionListController {
+
     @Autowired
     ElectionListRepository electionListRepository;
 
 
-    @RequestMapping(method= RequestMethod.GET)
-    List<Election> findAll() {
+    @RequestMapping(method = RequestMethod.GET)
+    List<Election> findElections() {
         return electionListRepository.findAll();
     }
 
-    @RequestMapping(method= RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     Election createElection(@RequestBody Election newElection) {
-
         return electionListRepository.save(newElection);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="/{electionId}")
+    Election getElection(@PathVariable Long electionId) {
+        return electionListRepository.findById(electionId).orElseThrow(() -> new ObjectNotFoundException(electionId, "election"));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value="/{electionId}")
+    Election editElection(@PathVariable Long electionId, @RequestBody Election newElection) {
+        return electionListRepository.findById(electionId)
+                .map(election -> {
+                        election.setName(newElection.getName());
+                        return electionListRepository.save(election);}).orElseGet(() -> {
+                        newElection.setId(electionId);
+                        return electionListRepository.save(newElection);});
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value="/{electionId}")
+    void deleteElection(@PathVariable Long electionId) {
+        electionListRepository.deleteById(electionId);
     }
 
 }
